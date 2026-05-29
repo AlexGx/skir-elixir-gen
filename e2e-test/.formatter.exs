45ad434efec92f15485e19b8e88941d174f2f215
Alex Gubarev <gubarev.alex@gmail.com>
2026-05-30 00:38:29 +0300

    wip

--- e2e-test/.formatter.exs
+++ e2e-test/.formatter.exs
@@ -1,16 +1,5 @@
-# Used by "mix format"
-locals_without_parens = [
-  method: 2,
-  method: 3,
-  field: 3,
-  variant: 2,
-  variant: 3,
-  removed: 1,
-  defconst: 3
-]
-
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["{mix,.formatter}.exs", "{test}/**/*.{ex,exs}"],
   locals_without_parens: locals_without_parens,
   export: [locals_without_parens: locals_without_parens]
 ]
